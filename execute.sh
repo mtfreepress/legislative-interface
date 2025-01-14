6ec7c33ce56151d27dcb9e2bd1604798a97e0591
Jacob Olness <jolness1@gmail.com>
2025-01-14 11:33:21 -0700

    tweaks

--- execute.sh
+++ execute.sh
@@ -13,26 +13,27 @@ sessionOrdinal=20251
 legislatureOrdinal=69
 
 
-# # get bill data json
-# python ./interface/get-bill-data.py $sessionId
+# get bill data json
+python ./interface/get-bill-data.py $sessionId
 
-# # get legislators
-# python ./interface/get-legislators.py
+# get legislators
+python ./interface/get-legislators.py
 
 # generate list of bills for input into other scripts
 python ./interface/generate-bill-list.py $sessionId
 
 # get vote data:
+python ./interface/get-votes-json.py
 
-
+#TODO: logic for `if sessionOrdinal is <20251, use this else use get-votes-json.py`
 # download PDFs - only needed for sessions prior to 2025:
 # python ./interface/get-pdf-votesheets.py --sessionId "$sessionId" --legislatureOrdinal "$legislatureOrdinal" --sessionOrdinal "$sessionOrdinal"
 
-# # parse vote counts from PDFs
-# python ./process/process-vote-pdfs.py $sessionId
+# parse vote counts from PDFs
+python ./process/process-vote-pdfs.py $sessionId
 
-# # process bill json into format we need
-# python ./process/process-bills.py $sessionId
+# process bill json into format we need
+python ./process/process-bills.py $sessionId
 
-# # process bill actions
-# python ./process/process-actions.py $sessionId
+# process bill actions
+python ./process/process-actions.py $sessionId
