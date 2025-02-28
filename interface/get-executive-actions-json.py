4ddac104335c583648d3713d65dc343d8148f6fe
Jacob Olness <jolness1@gmail.com>
2025-02-27 17:44:02 -0700

    changing max workers

--- interface/get-executive-actions-json.py
+++ interface/get-executive-actions-json.py
@@ -9,19 +9,26 @@ from requests.adapters import HTTPAdapter
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 LIST_BILLS_PATH = os.path.join(BASE_DIR, '../list-bills-2.json')
 
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Fetch and save executive actions data for bills.")
-    parser.add_argument('legislative_session', type=str, help="Legislative session identifier")
+    parser = argparse.ArgumentParser(
+        description="Fetch and save executive actions data for bills.")
+    parser.add_argument('legislative_session', type=str,
+                        help="Legislative session identifier")
     return parser.parse_args()
 
+
 def get_download_dir(legislative_session):
-    path = os.path.join(BASE_DIR, f'downloads/raw-{legislative_session}-executive-actions')
-    os.makedirs(path, exist_ok=True) 
+    path = os.path.join(
+        BASE_DIR, f'downloads/raw-{legislative_session}-executive-actions')
+    os.makedirs(path, exist_ok=True)
     return path
+
 
 def load_bills(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
+
 
 def fetch_data(url):
     session = requests.Session()
@@ -39,11 +46,13 @@ def fetch_data(url):
     response.raise_for_status()
     return response.json()
 
+
 def save_executive_actions_data(data, bill_type, bill_number, download_dir):
     file_name = f"{bill_type}-{bill_number}-executive-actions.json"
     file_path = os.path.join(download_dir, file_name)
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
+
 
 def fetch_and_save_executive_actions_data(bill, download_dir):
     bill_id = bill['id']
@@ -52,9 +61,11 @@ def fetch_and_save_executive_actions_data(bill, download_dir):
     try:
         executive_actions_url = f"https://api.legmt.gov/committees/v1/executiveActions/findByBillId?billId={bill_id}"
         executive_actions_data = fetch_data(executive_actions_url)
-        save_executive_actions_data(executive_actions_data, bill_type, bill_number, download_dir)
+        save_executive_actions_data(
+            executive_actions_data, bill_type, bill_number, download_dir)
     except requests.RequestException as e:
         print(f"Failed to fetch data for bill ID {bill_id}: {e}")
+
 
 def main():
     args = parse_arguments()
@@ -62,10 +73,12 @@ def main():
     download_dir = get_download_dir(legislative_session)
     bills = load_bills(LIST_BILLS_PATH)
 
-    with ThreadPoolExecutor(max_workers=15) as executor:
-        futures = [executor.submit(fetch_and_save_executive_actions_data, bill, download_dir) for bill in bills]
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [executor.submit(
+            fetch_and_save_executive_actions_data, bill, download_dir) for bill in bills]
         for future in as_completed(futures):
             future.result()
+
 
 if __name__ == "__main__":
     main()
